3800b5c27e328bea59ae06d61b00222776f22d78
IAndres98 <ivanandres0205@gmail.com>
2024-08-02 16:09:10 -0400

    Se agrega el Proyecto

---
+++ backend/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    jwtSecret: 'djbwhdhnudhbwnxjnwdhuwwdhwxndh',
+    jwtExpirySeconds: 3600,
+    apiKeys: {
+        openWeatherMap: '9f0699e75c0ca345057344fac67123d7',
+    },
+    db: {
+        host: 'localhost',
+        user: 'root',
+        password: 'ricardojosue2005',
+        database: 'meteorotep'
+    }
+};
